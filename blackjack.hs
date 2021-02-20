c044578490a6fb5dcf5f878a3ed3e3c589aa2ca6
Adrian Pikor <loler88@hotmail.ca>
2021-02-20 15:29:22 -0800

    Adjusted some comments explaining the state; changed Card type to be pair of two Char's (to allow J, Q, and K values)

--- blackjack.hs
+++ blackjack.hs
@@ -8,10 +8,6 @@ Very simple implentation of Blackjack/Twenty-One:
 	1) 2 players, human and computer (dealer)
 	2) Each player keeps drawing cards (face-up, i.e. visible) one at a time
 	3) If any player goes over 21 (sum of their card values), they lose
-	4) Card values:
-		- Ace: 1 or 11
-		- 2 - 10: same value as number
-		- Jack, Queen, King: 10
 -}
 
 
@@ -27,18 +23,14 @@ data State = State InternalState [Action] [Action]
 
 {-
 internal state consists of a 4-tuple with values:
-	- player pair:
-		- list of cards drawn
-		- bet amount
-	- CPU pair:
-		- list of cards drawn
-		- bet amount
+	- player's list of cards drawn
+	- CPU's list of cards drawn
 	- list of cards available to draw
 	- boolean indicating current player (True = player, False = CPU)
 -}
 type InternalState = ([Card], [Card], [Card], Bool)
 
-type Card = (Char, Integer)
+type Card = (Char, Char) -- (suit, value (1 - 10, J, Q, or K))
 
 type Player = State -> Action
 
@@ -69,7 +61,8 @@ blackjack act (State (pCards, cCards, deck, currPlayer) [] cAvailAct) =
 blackjack act (State (pCards, cCards, deck, currPlayer) pAvailAct []) =
 	ContinueGame (State (pCards, cCards, deck, not currPlayer) pAvailAct [])
 
--- action = hit (needs to use checkSum to check the sum of a player's card values after a card was drawn)
+-- action = hit (needs to use checkSum to check the sum of a player's card values after a card was drawn,
+--				 and drawFromDeck to randomly draw a card from the deck)
 --blackjack (Hit) (State (pState, cState, deck, currPlayer) pAvailAct cAvailAct) =
 --	ContinueGame (drawFromDeck (State (pState, cState, deck, currPlayer) pAvailAct cAvailAct))
 
@@ -81,8 +74,9 @@ blackjack (Stand) (State (pCards, cCards, deck, currPlayer) pAvailAct cAvailAct)
 
 {-----------HELPER FUNCTIONS-----------}
 
---drawFromDeck :: State -> State
---NEED TO DEFINE
+-- drawFromDeck :: State -> State
+-- drawFromDeck (State (pCards, cCards, deck, currPlayer) pAvailAct cAvailAct)
+--NEED TO DEFINE (cards need to be randomly drawn somehow)
 
 -- adds up values of a list of cards
 sumCards :: [Card] -> Integer
@@ -106,14 +100,14 @@ checkSum (State (pCards, cCards, deck, currPlayer) pAvailAct cAvailAct)
 --	'h' == hearts
 --	'c' == clubs
 fullDeck :: [Card]
-fullDeck = [('s',1), ('s',2), ('s',3), ('s',4), ('s',5), ('s',6), ('s',7),
-	('s',8), ('s',9), ('s',10), ('s',11), ('s',12), ('s',13), --spades
-	('d',1), ('d',2), ('d',3), ('d',4), ('d',5), ('d',6), ('d',7),
-	('d',8), ('d',9), ('d',10), ('d',11), ('d',12), ('d',13), --diamonds
-	('h',1), ('h',2), ('h',3), ('h',4), ('h',5), ('h',6), ('h',7),
-	('h',8), ('h',9), ('h',10), ('h',11), ('h',12), ('h',13), --hearts
-	('c',1), ('c',2), ('c',3), ('c',4), ('c',5), ('c',6), ('c',7),
-	('c',8), ('c',9), ('c',10), ('c',11), ('c',12), ('c',13)] --clubs
+fullDeck = [('s','1'), ('s','2'), ('s','3'), ('s','4'), ('s','5'), ('s','6'), ('s','7'),
+	('s','8'), ('s','9'), ('s','10'), ('s','J'), ('s','Q'), ('s','K'), --spades
+	('d','1'), ('d','2'), ('d','3'), ('d','4'), ('d','5'), ('d','6'), ('d','7'),
+	('d','8'), ('d','9'), ('d','10'), ('d','J'), ('d','Q'), ('d','K'), --diamonds
+	('h','1'), ('h','2'), ('h','3'), ('h','4'), ('h','5'), ('h','6'), ('h','7'),
+	('h','8'), ('h','9'), ('h','10'), ('h','J'), ('h','Q'), ('h','K'), --hearts
+	('c','1'), ('c','2'), ('c','3'), ('c','4'), ('c','5'), ('c','6'), ('c','7'),
+	('c','8'), ('c','9'), ('c','10'), ('c','J'), ('c','Q'), ('c','K')] --clubs
 
 -- start of new blackjack game
 newGame :: State
