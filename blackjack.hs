1f57b43e5322655e356cda7c0b1452481d73435a
Adrian Pikor <loler88@hotmail.ca>
2021-02-20 19:15:36 -0800

    Converted card values back to Integer's

--- blackjack.hs
+++ blackjack.hs
@@ -12,6 +12,9 @@ Very simple implentation of Blackjack/Twenty-One:
 
 
 
+{-----------IMPORTS-----------}
+import System.Random
+
 {-----------DATA TYPES-----------}
 
 -- ***the following data types are adapted from Dr. David Poole's "MagicSum.hs" example:
@@ -30,7 +33,8 @@ internal state consists of a 4-tuple with values:
 -}
 type InternalState = ([Card], [Card], [Card], Bool)
 
-type Card = (Char, Char) -- (suit, value (1 - 10, J, Q, or K))
+-- (suit, value (1 - 10, J, Q, or K))
+type Card = (Char, Integer)
 
 type Player = State -> Action
 
@@ -100,14 +104,14 @@ checkSum (State (pCards, cCards, deck, currPlayer) pAvailAct cAvailAct)
 --	'h' == hearts
 --	'c' == clubs
 fullDeck :: [Card]
-fullDeck = [('s','1'), ('s','2'), ('s','3'), ('s','4'), ('s','5'), ('s','6'), ('s','7'),
-	('s','8'), ('s','9'), ('s','10'), ('s','J'), ('s','Q'), ('s','K'), --spades
-	('d','1'), ('d','2'), ('d','3'), ('d','4'), ('d','5'), ('d','6'), ('d','7'),
-	('d','8'), ('d','9'), ('d','10'), ('d','J'), ('d','Q'), ('d','K'), --diamonds
-	('h','1'), ('h','2'), ('h','3'), ('h','4'), ('h','5'), ('h','6'), ('h','7'),
-	('h','8'), ('h','9'), ('h','10'), ('h','J'), ('h','Q'), ('h','K'), --hearts
-	('c','1'), ('c','2'), ('c','3'), ('c','4'), ('c','5'), ('c','6'), ('c','7'),
-	('c','8'), ('c','9'), ('c','10'), ('c','J'), ('c','Q'), ('c','K')] --clubs
+fullDeck = [('s',1), ('s',2), ('s',3), ('s',4), ('s',5), ('s',6), ('s',7),
+	('s',8), ('s',9), ('s',10), ('s',11), ('s',12), ('s',13), --spades
+	('d',1), ('d',2), ('d',3), ('d',4), ('d',5), ('d',6), ('d',7),
+	('d',8), ('d',9), ('d',10), ('d',11), ('d',12), ('d',13), --diamonds
+	('h',1), ('h',2), ('h',3), ('h',4), ('h',5), ('h',6), ('h',7),
+	('h',8), ('h',9), ('h',10), ('h',11), ('h',12), ('h',13), --hearts
+	('c',1), ('c',2), ('c',3), ('c',4), ('c',5), ('c',6), ('c',7),
+	('c',8), ('c',9), ('c',10), ('c',11), ('c',12), ('c',13)] --clubs
 
 -- start of new blackjack game
 newGame :: State
