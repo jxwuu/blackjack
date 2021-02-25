a0d43ed98111e0a2d1bdcad10b42d7f9a9ff4ca8
Victor Cheng <victorcheng0425@gmail.com>
2021-02-24 20:54:42 -0800

    AI

--- blackjack.hs
+++ blackjack.hs
@@ -13,7 +13,12 @@ Very simple implentation of Blackjack/Twenty-One:
 
 
 {-----------IMPORTS-----------}
-import System.Random
+-- import System.Random
+import System.IO
+import Text.Read (readMaybe)
+import Data.Maybe (fromJust)
+import Data.Char (isDigit)
+import Text.Read
 
 {-----------DATA TYPES-----------}
 
@@ -50,7 +55,7 @@ Actions:
 		- Stand = take no more cards for the round
 -}
 data Action = Hit Int
-			| Stand
+             | Stand
          deriving (Ord, Eq, Show)
 
 
@@ -60,22 +65,22 @@ data Action = Hit Int
 blackjack :: Game
 -- if no available actions for either player, do nothing and continue the game
 blackjack act (State (pCards, cCards, deck, currPlayer) False True) =
-	ContinueGame (State (pCards, cCards, deck, not currPlayer) False True)
+    ContinueGame (State (pCards, cCards, deck, not currPlayer) False True)
 blackjack act (State (pCards, cCards, deck, currPlayer) True False) =
-	ContinueGame (State (pCards, cCards, deck, not currPlayer) True False)
+    ContinueGame (State (pCards, cCards, deck, not currPlayer) True False)
 
 -- action = hit (needs to use checkSum to check the sum of a player's card values after a card was drawn,
 --				 and drawFromDeck to draw a card from the deck)
-blackjack (Hit n) (State (pCards, cCards, deck, currPlayer) pCanHit cCanHit)
-	| currPlayer = checkSum (State (newCard:pCards, cCards, newDeck, not currPlayer) pCanHit cCanHit)
-	| otherwise = checkSum (State (pCards, newCard:cCards, newDeck, not currPlayer) pCanHit cCanHit)
-		where
-			(newCard,newDeck) = drawFromDeck deck n
+blackjack (Hit n) (State (pCards, cCards, deck, currPlayer) pCanHit cCanHit) 
+    | currPlayer = checkSum (State (newCard:pCards, cCards, newDeck, not currPlayer) pCanHit cCanHit)
+    | otherwise = checkSum (State (pCards, newCard:cCards, newDeck, not currPlayer) pCanHit cCanHit)
+        where
+             (newCard,newDeck) = drawFromDeck deck n
 
 -- action = stand (sets player's boolean flag (pCanHit/cCanHit) to False)
 blackjack (Stand) (State (pCards, cCards, deck, currPlayer) pCanHit cCanHit)
-	| currPlayer = ContinueGame (State (pCards, cCards, deck, currPlayer) False cCanHit)
-	| otherwise = ContinueGame (State (pCards, cCards, deck, currPlayer) pCanHit False)
+    | currPlayer = ContinueGame (State (pCards, cCards, deck, currPlayer) False cCanHit)
+    | otherwise = ContinueGame (State (pCards, cCards, deck, currPlayer) pCanHit False)
 
 
 
@@ -93,9 +98,9 @@ sumCards ((_,val):tCard) = val + (sumCards tCard)
 -- checks whether a player has exceeded 21
 checkSum :: State -> Result
 checkSum (State (pCards, cCards, deck, currPlayer) pCanHit cCanHit)
-	| sumCards pCards > 21 = EndOfGame False newGame
-	| sumCards cCards > 21 = EndOfGame True newGame
-	| otherwise = ContinueGame (State (pCards, cCards, deck, currPlayer) pCanHit cCanHit)
+    | sumCards pCards > 21 = EndOfGame False newGame
+    | sumCards cCards > 21 = EndOfGame True newGame
+    | otherwise = ContinueGame (State (pCards, cCards, deck, currPlayer) pCanHit cCanHit)
 
 
 
@@ -112,3 +117,58 @@ fullDeck = [(suit,value) | suit <- ['s','d','h','c'], value <- [1..13]]
 -- start of new blackjack game
 newGame :: State
 newGame = State ([], [], fullDeck, True) True True
+
+{-----------USER INTERFACE-----------}
+type Bet = (Int, Int) --User's money, AI's money
+start :: Game -> IO Bet
+start game = 
+    do
+        putStrLn ("Game start! Welcome To BlackJack. Please enter the amount of money that you want to spend.")
+        line <- getLine
+        if  (all isDigit line)
+            then
+                --let x = 123
+                --return (read line :: Int, read line :: Int)
+                play game newGame (read line :: Int, read line :: Int)
+            else
+              do
+                putStrLn ("Not an Integer, return (0,0) ")
+                --line <- getLine
+                return (0, 0)
+play :: Game -> State -> Bet -> IO Bet
+play game newGame (umoney, aimoney) = 
+    do
+      putStrLn ("Who Starts? 0 = You, 1 = AI, 2 = quit.")
+      line <- getLine
+      if line == "2"
+         then
+             return (umoney, aimoney)
+      else if line == "1"
+         then 
+             return (0,0)
+      else
+             return (12,12)
+
+
+
+
+
+{-----------Start the program-----------}
+--start blackjack
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
